476ed6cef0e4626a3556b8e0017fb3fa070dd253
Cubeur-manchot <matthieu.aubert17@gmail.com>
2021-01-09 01:08:42 +0100

    Update badwords detection

--- badWords.js
+++ badWords.js
@@ -1,18 +1,18 @@
 "use strict";
 
 const badWords = [
-	"b(i|1)tt?e",
-	"teub",
-	"ch(i|1)bre",
-	"(c|k)ou(i|1)ll?e",
-	"(e|a)n(c|k)ul",
-	"(c|k)(o|0)nn?a(r|ss?e)",
-	"p(é|e)tass?e",
-	"putt?(e|a?(i|1))",
-	"sal(o|0)p",
-	"br(a|e)nll?(é|e)"
+	"b(i|1)tt?es?",
+	"teube?s?",
+	"ch(i|1)bre?s?",
+	"(c|k)ou(i|1)ll?es?",
+	"(e|a)n(c|k)ul(é|e)s?",
+	"(c|k)(o|0)nn?a(rd?|ss?e)s?",
+	"p(é|e)tass?es?",
+	"putt?(e|ain|1)s?",
+	"sal(o|0)pe?s?",
+	"br(a|e)nll?(é?e)s?"
 ];
 
-const badWordsRegex = new RegExp("^.*(" + badWords.join("|") + ").*$", "gi");
+const badWordsRegex = new RegExp("(^| |	)" + badWords.join("|") + "( |	|$)", "gi");
 
 module.exports = {badWords, badWordsRegex};
