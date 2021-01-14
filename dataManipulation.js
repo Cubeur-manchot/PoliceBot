d472b658e9e38bd6ebf7deb82f1b4395585846b2
Cubeur-manchot <matthieu.aubert17@gmail.com>
2021-01-14 23:59:37 +0100

    Create data substraction tool

--- dataManipulation.js
+++ dataManipulation.js
@@ -45,6 +45,82 @@ const parseDate = dateString => new Date(
 		dateString.substr(17,2) // seconds
 	);
 
+const getReadableDiffDate = (firstDate, secondDate) => {
+	let diffResultFirstLevel = getReadableDiffDateOneLevel(firstDate, secondDate);
+	if (diffResultFirstLevel.unit === "seconds") { // if only seconds, display only one level
+		return `${diffResultFirstLevel.diffTime} second${result.diffTime === 1 ? "" : "s"}`;
+	} else { // else, display two levels
+		let diffResultSecondLevel = getReadableDiffDateOneLevel(firstDate, diffResultFirstLevel.approximatedDate);
+		return `${diffResultFirstLevel.diffTime} ${diffResultFirstLevel.unit} and ${diffResultSecondLevel.diffTime} ${diffResultSecondLevel.unit}`;
+	}
+};
+
+const getReadableDiffDateOneLevel = (firstDate, secondDate) => {
+	let diffTimeInSeconds = Math.floor((firstDate - secondDate)/1000);
+	if (diffTimeInSeconds < 60) {
+		return {
+			diffTime: diffTimeInSeconds,
+			unit: `second${diffTimeInSeconds === 1 ? "" : "s"}`,
+			approximatedDate: new Date(secondDate.getTime() + diffTimeInSeconds*1000) // add seconds
+		};
+	} else if (diffTimeInSeconds < 3600) {
+		let diffTimeInMinutes = Math.floor(diffTimeInSeconds/60);
+		return {
+			diffTime: diffTimeInMinutes,
+			unit: `minute${diffTimeInMinutes === 1 ? "" : "s"}`,
+			approximatedDate: new Date(secondDate.getTime() + diffTimeInMinutes*60000) // add minutes
+		};
+	} else if (diffTimeInSeconds < 86400) {
+		let diffTimeInHours = Math.floor(diffTimeInSeconds/3600);
+		return {
+			diffTime: diffTimeInHours,
+			unit: `hour${diffTimeInHours === 1 ? "" : "s"}`,
+			approximatedDate: new Date(secondDate.getTime() + diffTimeInHours*3600000) // add hours
+		};
+	} else {
+		let secondDatePlusOneMonth = new Date(secondDate);
+		secondDatePlusOneMonth.setMonth(secondDatePlusOneMonth.getMonth() + 1);
+		if (firstDate < secondDatePlusOneMonth) {
+			let diffTimeInDays = Math.floor(diffTimeInSeconds/86400);
+			return {
+				diffTime: diffTimeInDays,
+				unit: `day${diffTimeInDays === 1 ? "" : "s"}`,
+				approximatedDate: new Date(secondDate.getTime() + diffTimeInDays*86400000) // add days
+			};
+		} else {
+			let secondDatePlusOneYear = new Date(secondDate);
+			secondDatePlusOneYear.setFullYear(secondDatePlusOneYear.getFullYear() + 1);
+			if (firstDate < secondDatePlusOneYear) {
+				let secondDatePlusSomeMonths = secondDatePlusOneMonth;
+				let monthCount = 0;
+				while (secondDatePlusSomeMonths < firstDate) { // look for the number of months
+					monthCount++;
+					secondDatePlusSomeMonths.setMonth(secondDatePlusSomeMonths.getMonth() + 1);
+				}
+				secondDatePlusSomeMonths.setMonth(secondDatePlusSomeMonths.getMonth() - 1);
+				return {
+					diffTime: monthCount,
+					unit: `month${monthCount === 1 ? "" : "s"}`,
+					approximatedDate: secondDatePlusSomeMonths // add months
+				};
+			} else {
+				let secondDatePlusSomeYears = new Date(secondDatePlusOneYear);
+				let yearCount = 0;
+				while (secondDatePlusSomeYears < firstDate) { // look for the number of years
+					yearCount++;
+					secondDatePlusSomeYears.setFullYear(secondDatePlusSomeYears.getFullYear() + 1);
+				}
+				secondDatePlusSomeYears.setFullYear(secondDatePlusSomeYears.getFullYear() - 1);
+				return {
+					diffTime: yearCount,
+					unit: `year${yearCount === 1 ? "" : "s"}`,
+					approximatedDate: secondDatePlusSomeYears
+				};
+			}
+		}
+	}
+};
+
 const infoTypeFromIdFirstLetter = {
 	i: "infractions",
 	w: "warns",
@@ -90,4 +166,4 @@ const groupElementsByMemberId = elementsArray => {
 	return result;
 };
 
-module.exports = {readInfoData, writeInfoData, getAvailableId, getReadableDate, removeData, groupElementsByMemberId};
+module.exports = {readInfoData, writeInfoData, getAvailableId, getReadableDate, getReadableDiffDate, parseDate, removeData, groupElementsByMemberId, infoTypeFromIdFirstLetter};
