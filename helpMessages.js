93fc1e3cd52ffdbaac3345a6c8c2faa36f6d08fb
Cubeur-manchot <matthieu.aubert17@gmail.com>
2021-01-17 19:24:57 +0100

    Update some help messages

--- helpMessages.js
+++ helpMessages.js
@@ -1,15 +1,15 @@
 "use strict";
 
 const addInfractionHelpMessage = "```\n&addInfraction <member> <type> // <commentary>```"
-	+ "`<member>` : identifies the member. Ex: Cubeur-manchot#7706, Cubeur-manchot, 7706, 217709941081767937."
+	+ "`<member>` : identifies the member. Ex: Cubeur-manchot#7706, Cubeur-manchot, 217709941081767937."
 	+ "\n`<type>` : the type of infraction. Ex: HS, Bad words, ..."
 	+ "\n`<commentary>` (optional) : gives more information about the infraction."
 	+ "\n\nExample : ```\n&addInfraction Cubeur-manchot#7706 HS répétitifs // c'est relou```";
 
-const addWarnHelpMessage = "```\n&warn <member> <reason> <infractionId> // <commentary>```"
-	+ "`<member>` : identifies the member. Ex: Cubeur-manchot#7706, Cubeur-manchot, 7706, 217709941081767937."
+const addWarnHelpMessage = "```\n&warn <member> <reason> <infractionsId> // <commentary>```"
+	+ "`<member>` : identifies the member. Ex: Cubeur-manchot#7706, Cubeur-manchot, 217709941081767937."
 	+ "\n`<reason>` : the reason of the warn. Ex: HS répétitifs, Bad words, ..."
-	+ "\n`<infractionId>` (optional) : the infraction(s) to be attached to the warn. Ex: i#1 i#3"
+	+ "\n`<infractionsId>` (optional) : the infraction(s) to be attached to the warn. Ex: i#1 i#3"
 	+ "\n`<commentary>` (optional) : gives more information about the warn."
 	+ "\n\nExample : ```\n&warn Cubeur-manchot#7706 HS répétitifs i#1 i#3 // c'est relou```";
 
