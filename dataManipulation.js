843707df6fc5dc8b7ff3ded52bafbc38e510b193
Cubeur-manchot <matthieu.aubert17@gmail.com>
2021-11-04 00:26:35 +0200

    Reorder functions

--- dataManipulation.js
+++ dataManipulation.js
@@ -5,6 +5,27 @@ const {google} = require("googleapis");
 
 const spreadsheetId = "1Fn144WOdpBKbktlesaU9NfEFZIbjLpTin77SaOrLj6M";
 
+const getAuth = () => new google.auth.GoogleAuth({keyFile: "credentials.json", scopes: "https://www.googleapis.com/auth/spreadsheets"});
+
+const getSpreadsheetsValues = async auth => google.sheets({version: "v4", auth: await auth.getClient()}).spreadsheets.values;
+
+const setupGoogleSheetsAPICredentials = () => {
+	fs.writeFile("credentials.json", JSON.stringify({
+		type: process.env.CREDENTIALS_type,
+		project_id: process.env.CREDENTIALS_project_id,
+		private_key_id: process.env.CREDENTIALS_private_key_id,
+		private_key: process.env.CREDENTIALS_private_key,
+		client_email: process.env.CREDENTIALS_client_email,
+		client_id: process.env.CREDENTIALS_client_id,
+		auth_uri: process.env.CREDENTIALS_auth_uri,
+		token_uri: process.env.CREDENTIALS_token_uri,
+		auth_provider_x509_cert_url: process.env.CREDENTIALS_auth_provider_x509_cert_url,
+		client_x509_cert_url: process.env.CREDENTIALS_client_x509_cert_url
+	}), function (err) {
+		if (err) {throw err;} else {console.log("Google spreadsheets credentials file written successfully !")}
+	});
+};
+
 const loadData = async tabName => {
 	let auth = getAuth();
 	return (await (await getSpreadsheetsValues(auth)).get({
@@ -14,13 +35,32 @@ const loadData = async tabName => {
 	})).data.values;
 };
 
-const convertDataObjectToTableData = (object, tabName) => {
-	return [tabName === "infractions" ? [object.id, object.memberId, object.date, object.type, object.commentary]
-		: tabName === "warns" ? [object.id, object.memberId, object.date, object.reason, object.commentary]
-			: tabName === "bans" ? [object.id, object.memberId, object.date, object.expirationDate, object.reason, object.commentary]
-				: tabName === "members" ? [object.memberId, object.username, object.tag]
-					: tabName === "discussions" ? [object.id, object.savingDate, object.action, object.channelId, convertMessageObjectListToString(object.messages)]
-						: []];
+const readInfoData = async infoType => {
+	let data = await loadData(infoType);
+	let elementList = [];
+	let headers = data[0];
+	let headersLength = headers.length;
+	for (let lineIndex = 1; lineIndex < data.length; lineIndex++) {
+		let element = {};
+		for (let columnIndex = 0; columnIndex < headersLength; columnIndex++) {
+			element[headers[columnIndex]] = data[lineIndex][columnIndex] ? data[lineIndex][columnIndex] : "";
+		}
+		elementList.push(element);
+	}
+	if (infoType === "members") { // members : hash to speed up access
+		let memberList = {};
+		for (let element of elementList) {
+			memberList[element.memberId] = element;
+		}
+		return memberList;
+	} else if (infoType === "discussions") { // discussions : parse message list with separator
+		for (let discussion of elementList) {
+			discussion.messages = convertStringToMessageObjectList(discussion.messages);
+		}
+		return elementList;
+	} else {
+		return elementList;
+	}
 };
 
 const appendData = async (newObject, tabName) => {
@@ -69,65 +109,6 @@ const eraseRawData = async (tabName, rawIndex) => {
 	}, undefined);
 };
 
-const getAuth = () => new google.auth.GoogleAuth({keyFile: "credentials.json", scopes: "https://www.googleapis.com/auth/spreadsheets"});
-
-const getSpreadsheetsValues = async auth => google.sheets({version: "v4", auth: await auth.getClient()}).spreadsheets.values;
-
-const setupGoogleSheetsAPICredentials = () => {
-	fs.writeFile("credentials.json", JSON.stringify({
-		type: process.env.CREDENTIALS_type,
-		project_id: process.env.CREDENTIALS_project_id,
-		private_key_id: process.env.CREDENTIALS_private_key_id,
-		private_key: process.env.CREDENTIALS_private_key,
-		client_email: process.env.CREDENTIALS_client_email,
-		client_id: process.env.CREDENTIALS_client_id,
-		auth_uri: process.env.CREDENTIALS_auth_uri,
-		token_uri: process.env.CREDENTIALS_token_uri,
-		auth_provider_x509_cert_url: process.env.CREDENTIALS_auth_provider_x509_cert_url,
-		client_x509_cert_url: process.env.CREDENTIALS_client_x509_cert_url
-	}), function (err) {
-		if (err) {throw err;} else {console.log("Google spreadsheets credentials file written successfully !")}
-	});
-};
-
-const readInfoData = async infoType => {
-	let data = await loadData(infoType);
-	let elementList = [];
-	let headers = data[0];
-	let headersLength = headers.length;
-	for (let lineIndex = 1; lineIndex < data.length; lineIndex++) {
-		let element = {};
-		for (let columnIndex = 0; columnIndex < headersLength; columnIndex++) {
-			element[headers[columnIndex]] = data[lineIndex][columnIndex] ? data[lineIndex][columnIndex] : "";
-		}
-		elementList.push(element);
-	}
-	if (infoType === "members") { // members : hash to speed up access
-		let memberList = {};
-		for (let element of elementList) {
-			memberList[element.memberId] = element;
-		}
-		return memberList;
-	} else if (infoType === "discussions") { // discussions : parse message list with separator
-		for (let discussion of elementList) {
-			discussion.messages = convertStringToMessageObjectList(discussion.messages);
-		}
-		return elementList;
-	} else {
-		return elementList;
-	}
-};
-
-const getAvailableId = async infoType => {
-	let dataOfThisType = await readInfoData(infoType);
-	let idWithoutIncrement = infoType[0].toLowerCase() + "#";
-	for (let increment = 1; increment < 100; increment++) {
-		if (!dataOfThisType.find(dataInstance => dataInstance.id === idWithoutIncrement + increment)) { // id is free
-			return idWithoutIncrement + increment;
-		}
-	}
-};
-
 const removeBulkData = async elementsIdList => {
 	let elementsIdGroupedByType = groupElementsIdByType(elementsIdList);
 	for (let type in elementsIdGroupedByType) {
@@ -137,6 +118,44 @@ const removeBulkData = async elementsIdList => {
 			if (index !== -1) {
 				await eraseRawData(type, index + 2); // +1 because line starts at 1 in Google Sheets, and +1 because of the header line
 			}
+		}
+	}
+};
+
+const convertDataObjectToTableData = (object, tabName) => {
+	return [tabName === "infractions" ? [object.id, object.memberId, object.date, object.type, object.commentary]
+		: tabName === "warns" ? [object.id, object.memberId, object.date, object.reason, object.commentary]
+			: tabName === "bans" ? [object.id, object.memberId, object.date, object.expirationDate, object.reason, object.commentary]
+				: tabName === "members" ? [object.memberId, object.username, object.tag]
+					: tabName === "discussions" ? [object.id, object.savingDate, object.action, object.channelId, convertMessageObjectListToString(object.messages)]
+						: []];
+};
+
+const separator = "¶";
+
+const convertMessageObjectListToString = messageObjectList => {
+	let messageStringList = [];
+	for (let messageObject of messageObjectList) {
+		messageStringList.push(messageObject.authorId + separator + messageObject.date + separator + messageObject.content);
+	}
+	return messageStringList.join(separator + separator);
+};
+
+const convertStringToMessageObjectList = messagesString => {
+	let messageObjectList = [];
+	for (let messageString of messagesString.split(separator + separator)) {
+		let [authorId, date, content] = messageString.split(separator);
+		messageObjectList.push({authorId, date, content});
+	}
+	return messageObjectList;
+};
+
+const getAvailableId = async infoType => {
+	let dataOfThisType = await readInfoData(infoType);
+	let idWithoutIncrement = infoType[0].toLowerCase() + "#";
+	for (let increment = 1; increment < 100; increment++) {
+		if (!dataOfThisType.find(dataInstance => dataInstance.id === idWithoutIncrement + increment)) { // id is free
+			return idWithoutIncrement + increment;
 		}
 	}
 };
@@ -168,28 +187,8 @@ const groupElementsByMemberId = elementsArray => {
 	return result;
 };
 
-const separator = "¶";
-
-const convertMessageObjectListToString = messageObjectList => {
-	let messageStringList = [];
-	for (let messageObject of messageObjectList) {
-		messageStringList.push(messageObject.authorId + separator + messageObject.date + separator + messageObject.content);
-	}
-	return messageStringList.join(separator + separator);
-};
-
-const convertStringToMessageObjectList = messagesString => {
-	let messageObjectList = [];
-	for (let messageString of messagesString.split(separator + separator)) {
-		let [authorId, date, content] = messageString.split(separator);
-		messageObjectList.push({authorId, date, content});
-	}
-	return messageObjectList;
-};
-
 module.exports = {
 	setupGoogleSheetsAPICredentials,
-	readInfoData, appendData, updateData,
-	removeBulkData,
+	readInfoData, appendData, updateData, removeBulkData,
 	getAvailableId, groupElementsByMemberId, groupElementsIdByType, infoTypeFromIdFirstLetter
 };
