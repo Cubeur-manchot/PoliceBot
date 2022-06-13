576135b2d8d2db8d2b0f3cfb6ecf9a08b2f5793a
Cubeur-manchot <matthieu.aubert17@gmail.com>
2022-06-13 23:16:59 +0200

    [Immature words] Asynchronize function

--- immatureWords.js
+++ immatureWords.js
@@ -2,7 +2,7 @@
 
 const {sendMessageToChannel} = require("./messages.js");
 
-const handleImmatureWords = message => {
+const handleImmatureWords = async message => {
     if (message.content.toLowerCase().startsWith("feur")
         || message.content.toLowerCase().includes("(feur)")) {
         sendMessageToChannel(message.channel, "Sérieux tu n'as rien de plus mature ? :unamused:");
