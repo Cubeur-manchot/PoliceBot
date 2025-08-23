2519b4503dfaa0dbb8216dd8e75bf2ea0108a389
Cubeur-manchot <matthieu.aubert17@gmail.com>
2025-08-23 19:22:20 +0200

    Simplify access to DiscordClientManager and DataManager in CommandHandlers

--- commandHandlers/commandHandler.js
+++ commandHandlers/commandHandler.js
@@ -5,6 +5,8 @@ import Command from "../command.js";
 export default class CommandHandler {
 	constructor(commandManager, commandName, commandContexts, commandDescription, commandOptions) {
 		this.commandManager = commandManager;
+		this.discordClientManager = this.commandManager.bot.discordClientManager;
+		this.dataManager = this.commandManager.bot.dataManager;
 		this.command = new Command(this, commandName, commandContexts, commandDescription, commandOptions);
 		this.commandName = commandName;
 	};
