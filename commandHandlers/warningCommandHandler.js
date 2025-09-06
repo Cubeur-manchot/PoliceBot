263cd93b432009dce65784a9ba37d004d6ebe761
Cubeur-manchot <matthieu.aubert17@gmail.com>
2025-09-07 00:24:39 +0200

    Implement user context mode for warning command

--- commandHandlers/warningCommandHandler.js
+++ commandHandlers/warningCommandHandler.js
@@ -4,6 +4,7 @@ import CommandHandler from "./commandHandler.js";
 import Command from "../command.js";
 
 export default class WarningCommandHandler extends CommandHandler {
+	static warningModalShowErrorMessage = "Une erreur s'est produite lors de l'affichage de la modale de détails. L'avertissement n'a pas pu être enregistré";
 	static warningAddErrorMessage = "Une erreur s'est produite lors de l'ajout de l'avertissement. Celui-ci n'a pas pu être enregistré";
 	static warningAddSuccessMessage = ":white_check_mark: L'avertissement a été enregistré.";
 	constructor(commandManager) {
@@ -12,7 +13,7 @@ export default class WarningCommandHandler extends CommandHandler {
 			"warning",
 			{
 				slash: true,
-				user: false,
+				user: true,
 				message: false
 			},
 			"Déclare un avertissement concernant un membre",
@@ -28,12 +29,42 @@ export default class WarningCommandHandler extends CommandHandler {
 					description: "Motif de l'avertissement",
 					required: true
 				}
+			],
+			[
+				{
+					name: "userId",
+					label: "Id de l'utilisateur",
+					placeholder: null,
+					isLong: false,
+				},
+				{
+					name: "reason",
+					label: "Motif",
+					placeholder: "Spam, troll, vulgarités, ...",
+					isLong: true
+				}
 			]
 		);
 	};
 	handleApplicationCommand = async interaction => {
+		if (interaction.isChatInputCommand()) { // slash command, contains all options
 			let {member, reason} = this.parseCommandOptions(interaction.options);
 			return await this.warnUser(member.id, reason);
+		} else { // user context command, contains only the user
+			let member = interaction.targetMember;
+			let modalTitle = `Avertissement de ${member.nickname ?? member.user.globalName} (@${member.user.username})`;
+			if (modalTitle.length > 45) {
+				modalTitle = `${modalTitle.slice(0, 42)}...`;
+			}
+			let modalFields = this.command.modalFields;
+			modalFields[0].initialValue = member.user.id;
+			return this.buildDiscordModal(modalTitle, modalFields);
+		}
+	};
+	handleModalSubmit = async interaction => {
+		let {userId, reason} = this.parseModalTextFields(interaction.fields);
+		return await this.warnUser(userId, reason);
+	};
 	warnUser = async (userId, reason) => {
 		await this.dataManager.addWarning({userId, reason, time: new Date()}, WarningCommandHandler.warningAddErrorMessage);
 		return WarningCommandHandler.warningAddSuccessMessage;
