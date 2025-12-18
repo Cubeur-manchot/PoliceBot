c1b4b95ec33dc9066e9d36e8b82ead0ec0d067ec
Cubeur-manchot <matthieu.aubert17@gmail.com>
2025-12-19 00:58:50 +0100

    Review error handling and check types in ListMapCache

--- dataManagers/listMapCache.js
+++ dataManagers/listMapCache.js
@@ -24,53 +24,61 @@ export default class ListMapCache extends BotHelper {
 	};
 	getEntries = keys => keys.map(key => this.data.get(key)).filter(Boolean).flat();
 	setEntries = map => {
+		if (!(map instanceof Map)) {
+			throw new Error(`Refused to set cache for data type "${this.dataType}", because the provided data is not a instance of map.`);
+		}
+		if (map.values().some(value => !Array.isArray(value))) {
+			throw new Error(`Refused to set cache for data type "${this.dataType}", because at least one of the values of the map is not an array.`);
+		}
 		this.data = map;
 		this.resetExpiration();
 		this.logger.info(`Cache for data type "${this.dataType}" has been set (${map.size} keys) successfully.`);
 	};
 	getEntry = key => this.data.get(key);
 	addEntry = (key, list) => {
-		if (!this.data.has(key)) {
-			this.data.set(key, list);
-			this.resetExpiration();
-			this.logger.info(`Cache for data type "${this.dataType}" has been extended with new entry (key = "${key}", list = "${JSON.stringify(list)}") successfully.`);
-		} else {
-			throw `Failed to add a new entry to the cache for data type "${this.dataType}", because the cache already contains the key "${key}".`;
+		if (!Array.isArray(list)) {
+			throw new Error(`Refused to add a new entry to the cache for data type "${this.dataType}", because the provided value is not an array.`);
 		}
+		if (this.data.has(key)) {
+			throw new Error(`Refused to add a new entry to the cache for data type "${this.dataType}", because the cache already contains the key "${key}".`);
+		}
+		this.data.set(key, list);
+		this.resetExpiration();
+		this.logger.info(`Cache for data type "${this.dataType}" has been extended with new entry (key = "${key}", list = "${JSON.stringify(list)}") successfully.`);
 	};
 	replaceEntry = (key, list) => {
-		if (this.data.has(key)) {
-			this.data.set(key, list);
-			this.resetExpiration();
-			this.logger.info(`Cache entry for data type "${this.dataType}" has been updated (key = "${key}", list = "${JSON.stringify(list)}") successfully.`);
-		} else {
-			throw `Failed to update the cache entry for data type "${this.dataType}", because the cache does not contain the key "${key}".`;
+		if (!Array.isArray(list)) {
+			throw new Error(`Refused to replace the entry in the cache for data type "${this.dataType}", because the provided value is not an array.`);
 		}
+		if (!this.data.has(key)) {
+			throw new Error(`Refused to replace the entry in the cache for data type "${this.dataType}", because the cache does not contain the key "${key}".`);
+		}
+		this.data.set(key, list);
+		this.resetExpiration();
+		this.logger.info(`Cache entry for data type "${this.dataType}" has been updated (key = "${key}", list = "${JSON.stringify(list)}") successfully.`);
 	};
 	removeEntry = key => {
-		if (this.data.has(key)) {
-			this.data.delete(key);
-			this.logger.info(`Cache entry for data type "${this.dataType}" has been removed (key = "${key}") successfully.`);
-		} else {
-			throw `Failed to remove the cache entry for data type "${this.dataType}", because the cache does not contain the key "${key}".`;
+		if (!this.data.has(key)) {
+			throw new Error(`Refused to remove the entry in the cache for data type "${this.dataType}", because the cache does not contain the key "${key}".`);
 		}
+		this.data.delete(key);
+		this.logger.info(`Cache entry for data type "${this.dataType}" has been removed (key = "${key}") successfully.`);
 	};
 	pushToEntry = (key, value) => {
-		if (this.data.has(key)) {
-			this.data.get(key).push(value);
-			this.resetExpiration();
-			this.logger.info(`Cache entry for data type "${this.dataType}" has been extended (key = "${key}", new value = "${JSON.stringify(value)}") successfully.`);
-		} else {
-			throw `Failed to push the value "${value}" to the cache entry for data type "${this.dataType}", because the cache does not contain the key "${key}".`;
+		if (!this.data.has(key)) {
+			throw new Error(`Refused to push the value "${value}" to the entry in the cache for data type "${this.dataType}", because the cache does not contain the key "${key}".`);
 		}
+		this.data.get(key).push(value);
+		this.resetExpiration();
+		this.logger.info(`Cache entry for data type "${this.dataType}" has been extended (key = "${key}", new value = "${JSON.stringify(value)}") successfully.`);s
 	};
 	safePushToEntry = (key, value) => {
-		if (this.data.has(key)) {
-			this.data.get(key).push(value);
-			this.resetExpiration();
-			this.logger.info(`Cache entry for data type "${this.dataType}" has been extended (key = "${key}", new value = "${JSON.stringify(value)}") successfully.`);
+		if (!this.data.has(key)) {
+			return; // do not create the key if it does not exist, because only full lists are allowed
 		}
-		// do not create the key if it does not exist, because only full lists are allowed
+		this.data.get(key).push(value);
+		this.resetExpiration();
+		this.logger.info(`Cache entry for data type "${this.dataType}" has been extended (key = "${key}", new value = "${JSON.stringify(value)}") successfully.`);
 	};
 	getKeys = condition =>
 		[...this.data]
