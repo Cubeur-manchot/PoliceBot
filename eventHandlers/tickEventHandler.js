64092a38e4636dddb364b15744a29e1ef4e3a8c0
Cubeur-manchot <matthieu.aubert17@gmail.com>
2026-03-08 20:37:26 +0100

    Purge info log messages older than the retention duration

--- eventHandlers/tickEventHandler.js
+++ eventHandlers/tickEventHandler.js
@@ -5,7 +5,21 @@ import EventHandler from "./eventHandler.js";
 export default class TickEventHandler extends EventHandler {
 	constructor(eventManager) {
 		super(eventManager, "tick");
+		this.retentionTimeMilliseconds = parseInt(process.env.LOG_INFO_RENTENTION_DURATION_DAYS) * 24 * 60 * 60 * 1000;
 	};
 	handleEvent = async () => {
+		let infoLogChannel = (await this.dataManager.getLogChannel("infoLog")).data;
+		let messagesToDelete;
+		try {
+			messagesToDelete = await this.discordActionManager.fetchMessagesBeforeTimestamp(infoLogChannel, new Date().getTime() - this.retentionTimeMilliseconds);
+		} catch (messagesFetchError) {
+			this.logger.error("Cannot get old info log messages to delete.", messagesFetchError);
+			messagesToDelete = [];
+		}
+		try {
+			this.discordActionManager.bulkDeleteMessages(infoLogChannel, messagesToDelete);
+		} catch (bulkDeleteError) {
+			this.logger.error("Cannot delete all old info log messages.", error);
+		}
 	};
 };
